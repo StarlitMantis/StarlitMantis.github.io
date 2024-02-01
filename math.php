2ff474496e0c137bb83074aaf9fd90c80018a3fb
Emma <emrdurso@gmail.com>
2024-02-01 10:53:28 -0800

    Adding VOC Content
    attaching photos, articles, docs, and videos relevant to the VOC article.

---
+++ math.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Math</title>
+<link href="supersheet.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<p class="center"><a href="https://starlitmantis.com/">Home</a> - - - - <a href="Gallery/gallery_home.html">Photos</a> - - - - <a href="Calc_God.html">Ti-84</a> - - - - <a href="blog.html">Blog</a></p>
+<div class="wrapper">
+		
+  <div class="box header">
+	  
+	  <!--Write Title Here-->
+	  <h1 class="center">Math Tutor Signup Form</h1>
+	  
+	</div>
+		
+  <div class="box sidebar superpad">
+	  <!--Sidebar-->
+	  	<hr>
+	  	<h2><a href="https://starlitmantis.com/">Home</a></h2>
+	  	<h2><a href="Gallery/gallery_home.html">Photos</a></h2>
+	  	<h2><a href="Calc_God.html">Ti-84</a></h2>
+	  	<h2><a href="blog.html">Blog</a></h2>
+		<hr>
+		</div>
+		
+  <div class="box content">
+	
+	<!--Write Content Here-->
+	  
+	  <form class="center">
+	  	<label for="fname">First Name:</label>
+	  	<input type="text" id="fname" name="fname"><br><br>
+		<label for="lname">Last Name:</label>
+	  	<input type="text" id="lname" name="lname"><br><br>
+	  </form>
+	  
+	  <div class="center">
+	  <button type="submit" class="center">Submit</button>
+	  </div>
+	</div>
+	
+  <div class="box footer">
+	<!--Footer-->
+	
+	</div>
+</div>
+	<p class="center"><a href="https://starlitmantis.com/">Home</a> - - - - <a href="Gallery/gallery_home.html">Photos</a> - - - - <a href="Calc_God.html">Ti-84</a> - - - - <a href="blog.html">Blog</a></p>
+</body>
+</html>
